de110fe7f697363f3464677ca8f32cb36c769db7
GuilhermeBronze <gui.broleze@gmail.com>
2026-06-09 00:01:30 -0300

    1

--- lanchonete-do-gordo/src/App.jsx
+++ lanchonete-do-gordo/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 
-import './App.css'
+import "./styles/App.css";
+import Header from './Header.jsx'
 
 function App() {
- 
 
-    
+  return (
+    //APP RETURN
+    <>
+      <Header />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ lanchonete-do-gordo/src/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+      <>
+        <div>Comandas</div>
+        <div>Mesas</div>
+        <div>Estoque</div>
+        <div>Produtos</div>
+      </>
+    );
+  }
+
+  export default Header;
