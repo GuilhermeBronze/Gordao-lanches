b04f3960824363ea1421227bca8428ccc24aa989
GuilhermeBronze <gui.broleze@gmail.com>
2026-06-22 22:41:28 -0300

    21

--- lanchonete-do-gordo/src/Elements/LeftMenu.jsx
+++ lanchonete-do-gordo/src/Elements/LeftMenu.jsx
@@ -4,14 +4,14 @@ function LeftMenu() {
   return (
     <div className="left-menu-container">
       <div>
-        <strong>*menu hambugue*</strong>
+        <span className="material-symbols-outlined">menu</span>
         <div className="left-menu-items">Arquivo</div>
         <div className="left-menu-items">Financeiro</div>
         <div className="left-menu-items">Vendas</div>
         <div className="left-menu-items">Configurações</div>
       </div>
       <br />
-      <div className="left-menu-items"><strong>*Logo*</strong></div>
+      <div className="left-menu-items">*Logo*</div>
       <div className="left-menu-items">Mesas</div>
       <div className="left-menu-items">Encomendas</div>
       <div className="left-menu-items">Cadastros</div>
