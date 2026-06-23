9985a1ebfad160fff7cf01e67f151f87a43f36fd
GuilhermeBronze <gui.broleze@gmail.com>
2026-06-23 09:03:13 -0300

    65

--- lanchonete-do-gordo/src/Elements/LeftMenu.jsx
+++ lanchonete-do-gordo/src/Elements/LeftMenu.jsx
@@ -1,29 +1,29 @@
-import { useState } from 'react'
-import '../styles/LeftMenu.css'
+import { useState } from "react";
+import "../styles/LeftMenu.css";
 
 function LeftMenu() {
   const [openedMenu, setOpenedMenu] = useState(false);
-  
+
   function toggleMenu() {
     setOpenedMenu(!openedMenu);
   }
-  
+
   return (
     <div className="left-menu-container">
-      <div>
-        <span className="material-symbols-outlined" onClick={toggleMenu}>menu</span>
-        
-        {openedMenu && (
-          <>
-        <div className="left-menu-items">Arquivo</div>
-        <div className="left-menu-items">Financeiro</div>
-        <div className="left-menu-items">Vendas</div>
-        <div className="left-menu-items">Configurações</div>
-          </>
-        )}
+      <button className="hidden-menu-button" onClick={toggleMenu}>
+        <span className="material-symbols-outlined">menu</span>
+      </button>
+
+      {openedMenu && (
+        <>
+          <div className="left-menu-items">Arquivo</div>
+          <div className="left-menu-items">Financeiro</div>
+          <div className="left-menu-items">Vendas</div>
+          <div className="left-menu-items">Configurações</div>
+        </>
+      )}
+
       
-      </div>
-      <br />
       <div className="left-menu-items">*Logo*</div>
       <div className="left-menu-items">Mesas</div>
       <div className="left-menu-items">Encomendas</div>
